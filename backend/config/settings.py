a209477b9ddc00e5c9a59351e9c568746676ce70
Chauhan Mahaveer <45894858+chauhanmahavir@users.noreply.github.com>
2023-06-14 22:31:41 -0400

    Delete backend directory

--- backend/config/settings.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseSettings
-
-class DBSettings(BaseSettings):
-    db_name: str = "illustrix"
-    db_host: str = "localhost"
-    db_port: int = 27017
-
-db_settings = DBSettings()
--- backend/services/db.py
+++
@@ -1,5 +0,0 @@
-from mongoengine import connect
-from ..config import db_settings
-
-def connect_mongo_db():
-    connect(db=db_settings.db_name, host=db_settings.db_host, port=db_settings.db_port)
