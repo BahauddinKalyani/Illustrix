38560af03148cea1f528f8fd8333a1ec9c19f51a
Chauhan Mahaveer <45894858+chauhanmahavir@users.noreply.github.com>
2023-07-18 19:34:14 -0400

    Update Image Model

--- backend/models/image.py
+++ backend/models/image.py
@@ -2,7 +2,7 @@ from mongoengine import Document, StringField, DateTimeField, EmailField
 from datetime import datetime
 
 class Image(Document):
-    user_id = EmailField(required=True)
+    email = EmailField(required=True)
     filename = StringField(required=True)
     url = StringField(required=True)
     created_at = DateTimeField(default = datetime.utcnow())
